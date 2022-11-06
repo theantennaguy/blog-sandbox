d962d3ae28e7a292c2198dc1450545e26b5e2647
Ricardo Goncalves <antenna.guy.blog@gmail.com>
2022-11-06 22:32:22 +0000

    Update measurement files

--- images/post15/results_plotter.py
+++ images/post15/results_plotter.py
@@ -7,18 +7,23 @@ from matplotlib.colors import LogNorm
 def polar_2_rectangular(mag, angles):
     return mag * np.exp(1j*angles)
 
-lna_cal = rf.Network('/Users/ricardogoncalves/Documents/GitHub/theantennaguy.github.io/images/post15/lna_sparams_cal.s2p')
+lna_cal = rf.Network('/Users/ricardogoncalves/Documents/GitHub/blog-sandbox/images/post15/lna_sparams_cal.s2p')
 lna_cal.name = 'LNA'
 lna_cal.frequency.unit = 'ghz'
 
-gd = abs(lna_cal.s21.group_delay) *1e9
+antenna = rf.Network('/Users/ricardogoncalves/Documents/GitHub/blog-sandbox/images/post15/gnss_antenna.s1p')
+
+#gd = abs(lna_cal.s21.group_delay) *1e9
+
+fig, ax = plt.subplots()
+antenna.plot_s_db()
 
 fig, ax = plt.subplots()
 lna_cal.plot_s_db()
 
-fig, ax = plt.subplots()
-lna_cal.plot(gd)
-plt.ylabel('Group Delay (ns)')
+#fig, ax = plt.subplots()
+#lna_cal.plot(gd)
+#plt.ylabel('Group Delay (ns)')
 
 plt.show()
 
